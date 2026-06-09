ae27fe6d745d437f30ce0c5ce852a405971c3475
Vladyslav <shmykov.vladyslav.1994@gmail.com>
2026-06-09 17:24:56 +0200

    fix build

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,32 +1,32 @@
-Rimport { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { useAuth } from "../../context/auth-context"
 
 function Header() {
-    const { user, logout } = useAuth()
-    const HEADER_LINKS = [
-        { to: '/make-new', label: 'Make new' },
-        { to: '/trainings', label: 'Trainings' },
-    ]
+  const { user, logout } = useAuth()
+  const HEADER_LINKS = [
+    { to: '/make-new', label: 'Make new' },
+    { to: '/trainings', label: 'Trainings' },
+  ]
   return (
     <header className="flex items-center justify-between bg-slate-800 text-white px-6 py-4">
-        <nav className="flex gap-4">
-            <Link to="/">
-                <img src="/src/shared/img/logo.png" alt="Logo" className="h-8" />
-            </Link>   
-          {links.map(link => (
-            !!user && (
-              <Link key={link.to} to={link.to} className="hover:text-blue-400 transition">
-                {link.label}
-              </Link>
-            )
-          ))}
-        </nav>
-        {user && <button
-        onClick={() =>{logout()}}
+      <nav className="flex gap-4">
+        <Link to="/">
+          <img src="/src/shared/img/logo.png" alt="Logo" className="h-8" />
+        </Link>
+        {links.map(link => (
+          !!user && (
+            <Link key={link.to} to={link.to} className="hover:text-blue-400 transition">
+              {link.label}
+            </Link>
+          )
+        ))}
+      </nav>
+      {user && <button
+        onClick={() => { logout() }}
         className="rounded bg-red-600 px-4 py-2 hover:bg-red-700 cursor-pointer">
-            Logout
-            </button>}
-      </header>
+        Logout
+      </button>}
+    </header>
   )
 }
 
